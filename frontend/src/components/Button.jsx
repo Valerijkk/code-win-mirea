3c21e651cb2ee104fad24b8f7d6a8bd7d0a232c1
f33lka <lloydxkir@gmail.com>
2025-05-19 17:20:44 +0300

    допили адаптивный дизайн, теперь хорошо отображаешься в виде на пол экрана

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -8,10 +8,10 @@ const Button = ({ text, help, image, index, isActive, toggleState }) => {
     };
 
     return (
-        <button class={`relative inline-block mx-1 w-auto h-full rounded-3xl border xl:text-xs 2xl:text-sm ${isActive ? 'bg-blue-700' : 'bg-neutral-700'} ${isActive ? 'border-blue-600' : 'border-neutral-500'} ${isActive ? 'text-blue-300' : 'text-white'}`} onClick={handleClick} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-            <div class="mx-2 flex">
+        <button class={`relative inline-block mx-1 w-auto h-full rounded-3xl border sm:text-xs md:text-xs lg:text-xs xl:text-xs 2xl:text-sm ${isActive ? 'bg-blue-700' : 'bg-neutral-700'} ${isActive ? 'border-blue-600' : 'border-neutral-500'} ${isActive ? 'text-blue-300' : 'text-white'}`} onClick={handleClick} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+            <div class="sm:mx-1 md:mx-1 lg:mx-1 xl:mx-2 2xl:mx-2 flex">
                 <div class="flex flex-col justify-center">
-                    <image class="h-3/4 w-5 bg-white"> {/* src={`${image}`} posle vstavki kartinki uberite "w-5" i postavte norm sootnoshenie */}
+                    <image class="h-3/4 sm:w-1 md:w-2 lg:w-3 xl:w-4 2xl:w-5 bg-white"> {/* src={`${image}`} posle vstavki kartinki uberite "w-5" i postavte norm sootnoshenie */}
                         
                     </image>
                 </div>
--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -4,10 +4,10 @@ const Image = ({ help, image, onClick }) => {
     const [isHovered, setIsHovered] = useState(false);
 
     return (
-        <div class="relative h-1/8 flex justify-center xl:text-xs 2xl:text-sm" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+        <div class="relative h-1/8 flex justify-center sm:text-xs md:text-xs lg:text-xs xl:text-xs 2xl:text-sm" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
             <div class="w-1/2 flex flex-col justify-center">
                 <button class="h-1/2" onClick={onClick}>
-                    <image class="text-neutral-300 ms:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl" src={image}>
+                    <image class="text-neutral-300 sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl" src={image}>
                         {image}
                     </image>
                 </button>
